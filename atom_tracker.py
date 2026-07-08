327d1e663cc6db20e1ccde5861868f534a65401c
adriangzlz97 <adrian.gonzalez.lopez@icm.uu.se>
2026-07-08 11:51:14 +0200

    Improved documentation and added main()

--- atom_tracker.py
+++ atom_tracker.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+"""
+atom_tracker.py - per-residue/atom coordinate changes between two aligned structures.
+
+Given two equivalent, pre-aligned pdb/cif files, report for each residue the
+largest per-atom displacement, the average atom displacement and the CA/C1'
+displacement. The table is sorted by the largest displacement first. Inputs must
+be aligned beforehand (e.g. in ChimeraX) if they do not come from the same refinement.
+"""
 from pdb_python_tools import Atom
 from pdb_python_tools import Residue
 from pdb_python_tools import load_residues
@@ -7,47 +15,50 @@ from pdb_python_tools import add_output_args
 from pdb_python_tools import write_table
 import argparse
 import sys
-# Check for flags
-parser = argparse.ArgumentParser(
-                    prog='track_xyz.py',
-                    description='Track xyz changes between two equivalent and aligned pdb/cif files',
-                    epilog='Usage: pdb1/cif1 pdb2/cif2 -arguments')
-parser.add_argument('pdb1', help='first coordinate file (pdb/cif)')
-parser.add_argument('pdb2', help='second coordinate file (pdb/cif)')
-parser.add_argument('-HET','--HETATM', action='store_true', dest='hetatm', help='include hetatms')
-parser.add_argument('-hy','--hydrogens', action='store_true', dest='hydrogens', help='include hydrogens')
-parser.add_argument('--min-change', type=float, default=0.01, dest='min_change',
-                    help='only report residues whose maximum displacement exceeds this value (default: 0.01)')
-add_output_args(parser)
-args = parser.parse_args()
-pdb1 = args.pdb1
-pdb2 = args.pdb2
-hetatm = args.hetatm
-hydrogens = args.hydrogens
-#  Check format and parse with appropriate function
-pdb1 = load_residues(pdb1, hetatm, hydrogens)
-pdb2 = load_residues(pdb2, hetatm, hydrogens)
 
-# Compare both pdbs
-compare_pdb_resi_xyz(pdb1,pdb2)
-for resi in pdb1:
-    resi.max_xyz = max(resi.atom_list, key=lambda x: x.xyz_change)
-    resi.average_xyz = sum(atom.xyz_change for atom in resi.atom_list) / len(resi.atom_list)
 
-pdb1.sort(key=lambda x: x.max_xyz.xyz_change, reverse=True)
-# Build the table, keeping only residues that moved more than --min-change
-header = ["Chain", "Residue", "Residue name", "Max_Distance", "Max_atom",
-          "Average_distance", "CA/C1'_distance"]
-rows = []
-for resi in pdb1:
-    if resi.max_xyz.xyz_change > args.min_change:
-        # Blank/NA when the residue has no real CA/C1' atom in both structures
-        ca = resi.CA.xyz_change if resi.CA.altid in ("CA", "C1'") else "NA"
-        rows.append([resi.chainid, resi.seqid, resi.restyp, resi.max_xyz.xyz_change,
-                     resi.max_xyz.altid, resi.average_xyz, ca])
+def main():
+    parser = argparse.ArgumentParser(
+        prog='atom_tracker.py',
+        description='Track xyz changes between two equivalent and aligned pdb/cif files',
+        epilog='Usage: pdb1/cif1 pdb2/cif2 -arguments')
+    parser.add_argument('pdb1', help='first coordinate file (pdb/cif)')
+    parser.add_argument('pdb2', help='second coordinate file (pdb/cif)')
+    parser.add_argument('-HET', '--HETATM', action='store_true', dest='hetatm', help='include hetatms')
+    parser.add_argument('-hy', '--hydrogens', action='store_true', dest='hydrogens', help='include hydrogens')
+    parser.add_argument('--min-change', type=float, default=0.01, dest='min_change',
+                        help='only report residues whose maximum displacement exceeds this value (default: 0.01)')
+    add_output_args(parser)
+    args = parser.parse_args()
 
-try:
-    write_table(header, rows, fmt=args.format, output=args.output, force=args.force,
-                precision=args.precision, full_precision=args.full_precision)
-except FileExistsError as e:
-    sys.exit(str(e))
+    # Parse both structures with the appropriate parser
+    pdb1 = load_residues(args.pdb1, args.hetatm, args.hydrogens)
+    pdb2 = load_residues(args.pdb2, args.hetatm, args.hydrogens)
+
+    # Compare both structures
+    compare_pdb_resi_xyz(pdb1, pdb2)
+    for resi in pdb1:
+        resi.max_xyz = max(resi.atom_list, key=lambda x: x.xyz_change)
+        resi.average_xyz = sum(atom.xyz_change for atom in resi.atom_list) / len(resi.atom_list)
+
+    pdb1.sort(key=lambda x: x.max_xyz.xyz_change, reverse=True)
+    # Build the table, keeping only residues that moved more than --min-change
+    header = ["Chain", "Residue", "Residue name", "Max_Distance", "Max_atom",
+              "Average_distance", "CA/C1'_distance"]
+    rows = []
+    for resi in pdb1:
+        if resi.max_xyz.xyz_change > args.min_change:
+            # Blank/NA when the residue has no real CA/C1' atom in both structures
+            ca = resi.CA.xyz_change if resi.CA.altid in ("CA", "C1'") else "NA"
+            rows.append([resi.chainid, resi.seqid, resi.restyp, resi.max_xyz.xyz_change,
+                         resi.max_xyz.altid, resi.average_xyz, ca])
+
+    try:
+        write_table(header, rows, fmt=args.format, output=args.output, force=args.force,
+                    precision=args.precision, full_precision=args.full_precision)
+    except FileExistsError as e:
+        sys.exit(str(e))
+
+
+if __name__ == "__main__":
+    main()
--- find_contacts.py
+++ find_contacts.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+"""
+find_contacts.py - inter-chain atom contacts for a given chain.
+
+Find all atoms of other chains within a cutoff distance of a chosen chain, using
+a scipy cKDTree. By default one contact per residue pair is reported (the
+shortest); use -a/--all to list every atom pair.
+"""
 from pdb_python_tools import Atom
 from pdb_python_tools import Residue
 from pdb_python_tools import load_residues
@@ -7,57 +14,55 @@ from pdb_python_tools import add_output_args
 from pdb_python_tools import write_table
 import argparse
 import sys
-import numpy as np
 
-# Check for flags
-parser = argparse.ArgumentParser(
-                    prog='find_contacts.py',
-                    description='Find possible contacts between chains for a given chain and within a given distance',
-                    epilog='Usage: pdb1/cif1 -arguments')
-parser.add_argument('pdb', help='coordinate file (pdb/cif)')
-parser.add_argument('-c','--chain', help='chain id to analyze', required=True)
-parser.add_argument('-d','--distance', help='distance to check',type=float, required=True)
-parser.add_argument('-HET','--HETATM', action='store_true', dest='hetatm', help='include hetatms')
-parser.add_argument('-hy','--hydrogens', action='store_true', dest='hydrogens', help='include hydrogens')
-parser.add_argument('-p','--polar_only', action='store_true', dest='polar', help='check only polar')
-parser.add_argument('-a','--all', action='store_true', dest='all', help='all output: display all atoms involved and distances')
-add_output_args(parser)
-args = parser.parse_args()
-pdb = args.pdb
-chain = args.chain
-distance = args.distance
-hetatm = args.hetatm
-hydrogens = args.hydrogens
-polar = args.polar
-show_all = args.all
 
-# Check format and parse with appropriate function
-pdb = load_residues(pdb, hetatm, hydrogens)
+def main():
+    parser = argparse.ArgumentParser(
+        prog='find_contacts.py',
+        description='Find possible contacts between chains for a given chain and within a given distance',
+        epilog='Usage: pdb1/cif1 -arguments')
+    parser.add_argument('pdb', help='coordinate file (pdb/cif)')
+    parser.add_argument('-c', '--chain', help='chain id to analyze', required=True)
+    parser.add_argument('-d', '--distance', help='distance to check', type=float, required=True)
+    parser.add_argument('-HET', '--HETATM', action='store_true', dest='hetatm', help='include hetatms')
+    parser.add_argument('-hy', '--hydrogens', action='store_true', dest='hydrogens', help='include hydrogens')
+    parser.add_argument('-p', '--polar_only', action='store_true', dest='polar', help='check only polar')
+    parser.add_argument('-a', '--all', action='store_true', dest='all',
+                        help='all output: display all atoms involved and distances')
+    add_output_args(parser)
+    args = parser.parse_args()
 
-# Find the inter-chain contacts within that distance (scipy cKDTree)
-atom_pairs = find_contacts_kdtree(pdb, distance, chain, polar)
+    # Parse with the appropriate parser
+    pdb = load_residues(args.pdb, args.hetatm, args.hydrogens)
 
-# Check if all output is requested
-if not show_all:
-    # Collapse to one contact per residue pair, keeping the shortest distance
-    # Key on the full residue identity of both partners (chain + seqid)
-    best = {}
-    for atom1, atom2, dist in atom_pairs:
-        key = (atom1.chainid, atom1.seqid, atom2.chainid, atom2.seqid)
-        if key not in best or dist < best[key][2]:
-            best[key] = [atom1, atom2, dist]
-    header = ["Residue1", "Residue1 number", "Chain2", "Residue2", "Residue2 number", "Distance"]
-    rows = [[atom1.restyp, atom1.seqid, atom2.chainid, atom2.restyp, atom2.seqid, dist]
-            for atom1, atom2, dist in best.values()]
-else:
-    header = ["Chain1", "Residue1", "Residue1 number", "Atom1",
-              "Chain2", "Residue2", "Residue2 number", "Atom2", "Distance"]
-    rows = [[atom1.chainid, atom1.restyp, atom1.seqid, atom1.altid,
-             atom2.chainid, atom2.restyp, atom2.seqid, atom2.altid, dist]
-            for atom1, atom2, dist in atom_pairs]
+    # Find the inter-chain contacts within that distance (scipy cKDTree)
+    atom_pairs = find_contacts_kdtree(pdb, args.distance, args.chain, args.polar)
 
-try:
-    write_table(header, rows, fmt=args.format, output=args.output, force=args.force,
-                precision=args.precision, full_precision=args.full_precision)
-except FileExistsError as e:
-    sys.exit(str(e))
+    # Check if all output is requested
+    if not args.all:
+        # Collapse to one contact per residue pair, keeping the shortest distance.
+        # Key on the full residue identity of both partners (chain + seqid).
+        best = {}
+        for atom1, atom2, dist in atom_pairs:
+            key = (atom1.chainid, atom1.seqid, atom2.chainid, atom2.seqid)
+            if key not in best or dist < best[key][2]:
+                best[key] = [atom1, atom2, dist]
+        header = ["Residue1", "Residue1 number", "Chain2", "Residue2", "Residue2 number", "Distance"]
+        rows = [[atom1.restyp, atom1.seqid, atom2.chainid, atom2.restyp, atom2.seqid, dist]
+                for atom1, atom2, dist in best.values()]
+    else:
+        header = ["Chain1", "Residue1", "Residue1 number", "Atom1",
+                  "Chain2", "Residue2", "Residue2 number", "Atom2", "Distance"]
+        rows = [[atom1.chainid, atom1.restyp, atom1.seqid, atom1.altid,
+                 atom2.chainid, atom2.restyp, atom2.seqid, atom2.altid, dist]
+                for atom1, atom2, dist in atom_pairs]
+
+    try:
+        write_table(header, rows, fmt=args.format, output=args.output, force=args.force,
+                    precision=args.precision, full_precision=args.full_precision)
+    except FileExistsError as e:
+        sys.exit(str(e))
+
+
+if __name__ == "__main__":
+    main()
